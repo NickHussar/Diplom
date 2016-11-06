afe8fff55877c15312bcf30ed29f45e5eb769c45
Plisskeens <koteleta@gmail.com>
2016-11-07 01:35:53 +0300

    New Design
    Progress in New Design

--- js/slider-range.js
+++ js/slider-range.js
@@ -1,12 +1,13 @@
 $( function() {
   $( "#slider-range" ).slider({
+    step: 0.1,
     range: true,
-    min: 0,
-    max: 500,
-    values: [ 75, 300 ],
+    min: 2.4,
+    max: 6.8,
+    values: [ 2.4, 6.8 ],
     slide: function ( event, ui ) {
-      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      $( "#amount" ).val( ui.values[ 0 ] + " ' - " + ui.values[ 1 ] + " '");
     }
   });
-  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + " ' - " + $( "#slider-range" ).slider( "values", 1 ) + " '");
 });
